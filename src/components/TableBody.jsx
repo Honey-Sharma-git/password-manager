bad05cd27c7cdc58e52f073e3029d459d9654a86
Honey-sharma-git <honeysharmaemail@gmail.com>
2025-03-27 17:04:07 +0530

    Add delete functionality

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -2,7 +2,31 @@ import { FaShareAlt } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { MdUpdate } from "react-icons/md";
 import { Password } from "./Password";
+import { baseURL } from "../utils/constant";
+import axios from "axios";
+import { addCredential } from "../redux/slice/addCredentialSlice";
+import { useDispatch } from "react-redux";
+
 export const TableBody = ({ item, index }) => {
+  const dispatch = useDispatch();
+  function deleteDomain(itemID) {
+    const answer = window.confirm("Do you really want to delete?");
+    if (answer) {
+      console.log(itemID);
+      const response = axios.post(
+        `${baseURL}/v1/deleteDomain`,
+        { id: itemID },
+        {
+          headers: {
+            Authorization: `Bearer ${JSON.parse(
+              localStorage.getItem("token")
+            )}`,
+          },
+        }
+      );
+      dispatch(addCredential());
+    }
+  }
   return (
     <tr key={item._id}>
       <td className="text-left border py-2 px-2 text-gray-500 ">{index}</td>
@@ -26,7 +50,11 @@ export const TableBody = ({ item, index }) => {
           <button>
             <FaShareAlt className="hover:text-gray-950 text-gray-500 cursor-pointer" />
           </button>
-          <button>
+          <button
+            onClick={() => {
+              deleteDomain(item._id);
+            }}
+          >
             <MdDelete className="hover:text-gray-950 text-gray-500 cursor-pointer" />
           </button>
           <button>
