c7707b710c18b67cdd3797e32299f2395a2ca244
Honey-sharma-git <honeysharmaemail@gmail.com>
2025-03-27 16:39:38 +0530

    Add Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,12 @@
 import { AddCredential } from "../components/AddCredential";
+import { Credentials } from "../components/Credentials";
+import { Navbar } from "../components/Navbar";
 export const Dashboard = () => {
   return (
     <main>
+      <Navbar />
       <AddCredential />
+      <Credentials />
     </main>
   );
 };
