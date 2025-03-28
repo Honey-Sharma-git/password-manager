570a259f3beb573401bb0266d3f0a1184b22b20a
Honey-sharma-git <honeysharmaemail@gmail.com>
2025-03-28 13:55:29 +0530

    Add username search bar

--- src/components/Credentials.jsx
+++ src/components/Credentials.jsx
@@ -5,13 +5,13 @@ import { TableBody } from "./TableBody";
 import { TableHeader } from "./TableHeader";
 import { useDispatch, useSelector } from "react-redux";
 import { setDomains } from "../redux/slice/domainsSlice";
-
+import { FaSearch } from "react-icons/fa";
 export const Credentials = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const dispatch = useDispatch();
   const domains = useSelector((state) => {
     return state.domains;
   });
-  const [userDetails, setUserDetails] = useState({}); //Needs to update it in a/c modal.
   const data = useSelector((state) => {
     return state.addCredential;
   });
@@ -31,7 +31,24 @@ export const Credentials = () => {
     getData();
   }, [data]);
 
- 
+  function searchUserName(searchQuery) {
+    // const searchQuery = "honey";
+    if (!searchQuery) {
+      getData();
+    } else {
+      dispatch(
+        setDomains([
+          ...domains.filter((item) => {
+            if (
+              item.userName.toLowerCase().includes(searchQuery.toLowerCase())
+            ) {
+              return true;
+            }
+          }),
+        ])
+      );
+    }
+  }
 
   const credentialElement = domains.map((item, index) => {
     return <TableBody key={item._id} item={item} index={index} />;
@@ -44,6 +61,20 @@ export const Credentials = () => {
       }
     >
       <div className="overflow-y-auto h-82 relative w-full  flex flex-col items-stretch">
+        <div className="relative rounded-lg">
+          <div className="absolute text-2xl  top-1/2 -translate-y-1/2 pl-2">
+            <FaSearch />
+          </div>
+          <input
+            className="p-2 w-full pl-10 rounded-lg"
+            type="search"
+            placeholder="Search usernames"
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+              searchUserName(e.target.value);
+            }}
+          />
+        </div>
         <table>
           <TableHeader domains={domains} />
           <tbody>{credentialElement}</tbody>
